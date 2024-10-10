4b4993e8a64c533cbc1b59dc9809ca615586b940
Ismail <ismail.khlif@esprit.tn>
2024-10-10 21:28:02 +0100

    initial Project

---
+++ SolarChain/src/main/java/tn/solarchain/SolarChainApplication.java
@@ -0,0 +1,13 @@
+package tn.solarchain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolarChainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolarChainApplication.class, args);
+    }
+
+}
---
+++ SolarChain/src/test/java/tn/solarchain/SolarChainApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.solarchain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolarChainApplicationTests {
+/*
+    @Test
+    void contextLoads() {
+    }
+*/
+}
